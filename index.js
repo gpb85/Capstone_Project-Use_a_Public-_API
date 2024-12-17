feb185e69a9721d4c0b3ee4f4185e6a3facc8ab3
gpd85 <giorgos_bithas@hotmail.com>
2024-12-17 16:15:21 +0200

    second commit

--- index.js
+++ index.js
@@ -16,23 +16,43 @@ app.get("/", async (req, res) => {
     city: "Enter City",
     humidity: "--",
     wind: "--",
+    WheatherDescription: "Clear",
+    isDay: true,
   });
 });
 
 app.post("/", async (req, res) => {
   const city = req.body.city;
   try {
-    const response = await axios.get(
+    const weatherResponse = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
     );
-    console.log(response);
 
-    let result = response.data;
+    let result = weatherResponse.data;
+    //console.log(result);
+
+    const lon = result.coord.lon;
+    const lat = result.coord.lat;
+
+    const airResponse = await axios.get(
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    );
+
+    const airQuality = airResponse.data.list[0].main.aqi;
+    console.log(airQuality);
+
+    const currentTime = Math.floor(Date.now() / 1000); //current time in UNIX time
+    const sunrise = result.sys.sunrise;
+    const sunset = result.sys.sunset;
+    const isDay = currentTime >= sunrise && currentTime <= sunset;
+
     res.render("index.ejs", {
       temp: Math.round(result.main.temp),
       city: result.name,
       humidity: Math.round(result.main.humidity),
       wind: Math.round(3.6 * result.wind.speed),
+      weatherDescription: result.weather[0].description || "Clear",
+      isDay: isDay,
     });
   } catch (error) {
     console.error("Error sending data", error);
@@ -41,6 +61,8 @@ app.post("/", async (req, res) => {
       city: "City not found!",
       humidity: "--",
       wind: "--",
+      weatherDescription: "Error",
+      isDay: true,
     });
   }
 });
